1a7e387d09c2b9b09adb81825ce8ffcb21b607c6
권준혁 <robot0004@gmail.com>
2022-12-10 11:18:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+## 그리디 알고리즘 예제 - 거스름돈 ##
+n = int(input()) #n = 거슬러 줘야 할 돈
+count = 0 #최종 동전 개수
+
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+  count += n // coin
+  n %= coin
+
+print(count)
